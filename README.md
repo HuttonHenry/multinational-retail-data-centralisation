57cde7953f27486cd2242656d513b582e1605a19
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-26 20:30:55 +0000

    Got postgres working

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+# H1
+Data Centralisation Project - Jan, 2023.
+
+
 # multinational-retail-data-centralisation
+
+
+Code created using Python 
+
+Uses SQL Alchemy for SQL connectivity
+https://docs.sqlalchemy.org/en/20/core/engines.html
+pip install sqlalchemy
+
+
+Postgres Connection
+I needed to change the YAML file, as it used the same variable for both the postgres database type and databaase name, but the type had changed to 'postgresql'.  
+
+I also had to install <conda install psycopg2>
+https://anaconda.org/anaconda/psycopg2
+
+
--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,24 @@
+import yaml
+from sqlalchemy import create_engine
+
 class DatabaseConnector:
-    
+    def read_db_creds(self):
+        with open('db_creds.yaml','r') as f:
+            self.creds = yaml.safe_load(f)
+        return self.creds
+
+    def init_db_engine(self):
+        creds = self.read_db_creds()
+        conn_string = (f"{creds['RDS_DBTYPE']}://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']/}/{creds['RDS_DATABASE']}"
+        print(conn_string)
+        print("Conencting to the ENGINE dude!")
+        try:
+            self.engine = create_engine(conn_string)
+        except:
+            print("Issue connecting to the database dudes.")
+        else:
+            print("Connected, no probs dudes.")
+        return self.engine
+
+db = DatabaseConnector()
+engine = db.init_db_engine()
