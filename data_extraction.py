be9b43f2d1e86e7668b79d0b002a3028ac450e23
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-31 20:22:13 +0000

    ReadPF to DataFrame

--- data_extraction.py
+++ data_extraction.py
@@ -1,1 +1,8 @@
+import tabula 
+
 class DataExtractor:
+    def retrieve_pdf_data(self, PDFLink):
+        dfs = tabula.read_pdf("https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf")
+        return dfs
+
+
